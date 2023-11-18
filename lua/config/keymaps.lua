61f44778d6229d257097afd5214dac4d616a12ce
Jean-Marc Barozet <jbarozet@gmail.com>
2023-11-18 18:33:43 +0100

    add window management keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -28,9 +28,4 @@ keymap.set("n", "sh", "<C-w>h")
 keymap.set("n", "sk", "<C-w>k")
 keymap.set("n", "sj", "<C-w>j")
 keymap.set("n", "sl", "<C-w>l")
-
--- Resize window
-keymap.set("n", "<C-w><left>", "<C-w><")
-keymap.set("n", "<C-w><right>", "<C-w>>")
-keymap.set("n", "<C-w><up>", "<C-w>+")
-keymap.set("n", "<C-w><down>", "<C-w>-")
+keymap.set("n", "sc", "<C-w>c")
